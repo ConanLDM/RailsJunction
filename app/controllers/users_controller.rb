5930acd9a4da982fc56b8578f253229571f4af1c
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-02-21 19:58:42 +0000

    Sidekiq Setup with a First User job
    Installed Sidekiq 7.2.2 and activated it via adding a
    confirmation line to the application.rb file that sidekiq
    will be the job adapter being used from this point
    onwards.
    
    Also setup the first user job in a Sidekiq user insertion
    class (placeholder for now).
    
    Users controller create action updated with the new
    Sidekiiq user insertion job method.
    
    Created a Procfile.dev file containing instructions for
    Sidekiq running in a local development environment.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     UserMailer.with(user: @user).welcome_email.deliver_later
 
+    UserInsertionWorker.perform_async(users)
+
     redirect_to @user
   end
 end
---
+++ app/sidekiq/user_insertion_job.rb
@@ -0,0 +1,9 @@
+class UserInsertionWorker
+  include Sidekiq::Worker
+
+  def perform(users)
+    users.each do |user|
+      User.create!(name: user.name, email: user.email)
+    end
+  end
+end
