6724617c5d1c541e7e58600090ade6af8f53a8e8
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-03-04 18:12:07 +0000

    Junction Create Form Created, Updated Related Files
    Created a Bootstrap Simple Form for creating a Junction,
    along with a field for the location and a selector of
    already existing cities for the city the junction and location
    is attributed to.
    
    Updated the junction model to include a junction attribute
    for using junctions as a field in the form and in general
    in the application going forward.
    
    Not fully working as of this commit, as the junction
    and city fields are viewable and editable but not
    displaying in the frontend in the index view when
    created.

--- app/controllers/junctions_controller.rb
+++ app/controllers/junctions_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class JunctionsController < ApplicationController # rubocop:todo Style/Documentation
+
+  def new
+    @junction = Junction.new
+  end
+
   def create
     @junction = Junction.new(junction_params)
+
+    @junction.city_id = params[:junction][:city_id]
 
     if @junction.save
       redirect_to junctions_path, notice: 'Junction was successfully created.'
@@ -18,6 +25,6 @@ class JunctionsController < ApplicationController # rubocop:todo Style/Documenta
   private
 
   def junction_params
-    params.require(:junction).permit(:name, :location)
+    params.require(:junction).permit(:location, :city_id)
   end
 end
--- app/models/junction.rb
+++ app/models/junction.rb
@@ -3,5 +3,6 @@
 class Junction < ApplicationRecord # rubocop:todo Style/Documentation
   belongs_to :city
 
+  attribute :junction
   attribute :location
 end
