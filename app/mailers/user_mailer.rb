98c2587f23712db89add8313906cd75f7d91207f
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-03-08 19:49:34 +0000

    Confirmation Mailer Setup, Current Model, Authentication Concern and other Existing Files Updated
    Confirmation mailer html and text view files created.
    
    Updated the development and test environment files to be able to
    handle action mailer default URL options.
    
    DB migrated and schema updated following current model and related files
    generated, along witrh an authentication.rb created in the controller's
    concerns folder.
    
    Users controller updated to allow the email parameter in the private parameters
    requirement method, however with both email and name now in this method it
    will likely be updated further moving forward.
    
    Additional create variables related to the confirmation email created to
    allow confirmation emails to be created and sent to new sign up users.
    
    User model updated to make confirmation emails a one time use and the
    confirmation token for them prevents reusage.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer # rubocop:todo Style/Documentation
-  default from: 'generic@generic.com'
+  default from: User::MAILER_FROM_EMAIL
 
   def welcome_email
     @user = params[:user]
     @url  = 'railsjunction.com/login'
+    # @confirmation_token = confirmation_token
+
     mail(to: @user.email, subject: 'Welcome to Rails Junction')
   end
 end
