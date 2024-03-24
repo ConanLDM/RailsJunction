613c037a34d63410ff4a21dab149187f7514c724
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2024-03-24 14:20:18 +0000

    Updated Sidekiq Worker and Application Files to Fix GitHub Actions Errors
    Renamed class of preexisting Sidekiq worker from UserInsertionWorker to UserInsertionJob.
    
    Provided an file autoload path to the application.rb file to include custom directories
    not in the gitignore file in the autoload paths.

--- app/sidekiq/user_insertion_job.rb
+++ app/sidekiq/user_insertion_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UserInsertionWorker # rubocop:todo Style/Documentation
+class UserInsertionJob # rubocop:todo Style/Documentation
   include Sidekiq::Worker
 
   def perform(users)
